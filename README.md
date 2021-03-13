a42add942d881fd7e1628343f518582f552ac428
Njood <njoodalshaalan.66@gmail.com>
2021-03-13 13:36:45 +0300

    v1

---
+++ README.md
@@ -0,0 +1,3 @@
+Please use the postman collection to try the endpoints.
+also please do not forget to export the tokens before testing the app :)
+thanks 
--- app.py
+++ app.py
@@ -233,4 +233,4 @@ app = create_app()
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='https://njood-capstone.herokuapp.com', port=8080, debug=True)
